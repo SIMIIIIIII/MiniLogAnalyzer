812ee9762df1633ebfe40a163f63e4249241b012
SIM <simeonlama00@gmail.com>
2026-09-24 17:53:04 +0200

    fix: correct values collected

--- perf.sh
+++ perf.sh
@@ -21,20 +21,79 @@ for i in $(seq 1 10); do
     { perf stat ./mininloganalyzer_v2 access_1M.log > /dev/null ; } 2>> perfs/perfs_v2.txt
 done
 
-TIME_V0="$(grep 'time elapsed' perfs/perfs_v0.txt | grep -oE '[0-9]+,[0-9]+' | tr ',' '.' | awk '{sum += $1; n++} END {print sum/n}')"
-TIME_V1="$(grep 'time elapsed' perfs/perfs_v1.txt | grep -oE '[0-9]+,[0-9]+' | tr ',' '.' | awk '{sum += $1; n++} END {print sum/n}')"
-TIME_V1B="$(grep 'time elapsed' perfs/perfs_v1b.txt | grep -oE '[0-9]+,[0-9]+' | tr ',' '.' | awk '{sum += $1; n++} END {print sum/n}')"
-TIME_V2="$(grep 'time elapsed' perfs/perfs_v2.txt | grep -oE '[0-9]+,[0-9]+' | tr ',' '.' | awk '{sum += $1; n++} END {print sum/n}')"
+TIME_V0="$(
+    grep 'time elapsed' perfs/perfs_v0.txt |
+    grep -oE '[0-9]+,[0-9]+' |
+    tr ',' '.' |
+    awk '{sum += $1; n++} END {print sum/n}'
+)"
+TIME_V1="$(
+    grep 'time elapsed' perfs/perfs_v1.txt |
+    grep -oE '[0-9]+,[0-9]+' | tr ',' '.' |
+    awk '{sum += $1; n++} END {print sum/n}'
+)"
+TIME_V1B="$(
+    grep 'time elapsed' perfs/perfs_v1b.txt |
+    grep -oE '[0-9]+,[0-9]+' |
+    tr ',' '.' |
+    awk '{sum += $1; n++} END {print sum/n}'
+)"
+TIME_V2="$(
+    grep 'time elapsed' perfs/perfs_v2.txt |
+    grep -oE '[0-9]+,[0-9]+'|
+    tr ',' '.' |
+    awk '{sum += $1; n++} END {print sum/n}'
+)"
 
-IPC_V0="$(grep 'cpu_core/instructions/u' perfs/perfs_v0.txt | awk '{print $1}' | tr -d '.' | awk '{sum += $1; n++} END {printf "%.0f\n", sum/n}')"
-IPC_V1="$(grep 'cpu_core/instructions/u' perfs/perfs_v1.txt | awk '{print $1}' | tr -d '.' | awk '{sum += $1; n++} END {printf "%.0f\n", sum/n}')"
-IPC_V1B="$(grep 'cpu_core/instructions/u' perfs/perfs_v1b.txt | awk '{print $1}' | tr -d '.' | awk '{sum += $1; n++} END {printf "%.0f\n", sum/n}')"
-IPC_V2="$(grep 'cpu_core/instructions/u' perfs/perfs_v2.txt | awk '{print $1}' | tr -d '.' | awk '{sum += $1; n++} END {printf "%.0f\n", sum/n}')"
+IPC_V0="$(
+    grep 'cpu_core/instructions/u' perfs/perfs_v0.txt |
+    awk '{print $4}' |
+    tr ',' '.' |
+    awk '{sum += $1; n++} END {printf sum/n}'
+)"
+IPC_V1="$(
+    grep 'cpu_core/instructions/u' perfs/perfs_v1.txt |
+    awk '{print $4}' |
+    tr ',' '.' |
+    awk '{sum += $1; n++} END {printf sum/n}'
+)"
+IPC_V1B="$(
+    grep 'cpu_core/instructions/u' perfs/perfs_v1b.txt |
+    awk '{print $4}' |
+    tr ',' '.' |
+    awk '{sum += $1; n++} END {printf sum/n}
+')"
+IPC_V2="$(
+    grep 'cpu_core/instructions/u' perfs/perfs_v2.txt |
+    awk '{print $4}' |
+    tr ',' '.' |
+    awk '{sum += $1; n++} END {printf sum/n}'
+)"
 
-CM_V0="$(grep 'cpu_core/branch-misses/u' perfs/perfs_v0.txt | awk '{print $1}' | tr -d '.' | awk '{sum += $1; n++} END {printf "%.0f\n", sum/n}')"
-CM_V1="$(grep 'cpu_core/branch-misses/u' perfs/perfs_v1.txt | awk '{print $1}' | tr -d '.' | awk '{sum += $1; n++} END {printf "%.0f\n", sum/n}')"
-CM_V1B="$(grep 'cpu_core/branch-misses/u' perfs/perfs_v1b.txt | awk '{print $1}' | tr -d '.' | awk '{sum += $1; n++} END {printf "%.0f\n", sum/n}')"
-CM_V2="$(grep 'cpu_core/branch-misses/u' perfs/perfs_v2.txt | awk '{print $1}' | tr -d '.' | awk '{sum += $1; n++} END {printf "%.0f\n", sum/n}')"
+CM_V0="$(
+    grep 'cpu_core/branch-misses/u' perfs/perfs_v0.txt |
+    awk '{print $4}'|
+    tr ',' '.' |
+    awk '{sum += $1; n++} END {printf sum/n}'
+ )"
+CM_V1="$(
+    grep 'cpu_core/branch-misses/u' perfs/perfs_v1.txt |
+    awk '{print $4}'|
+    tr ',' '.' |
+    awk '{sum += $1; n++} END {printf sum/n}'
+ )"
+CM_V1B="$(
+    grep 'cpu_core/branch-misses/u' perfs/perfs_v1b.txt |
+    awk '{print $4}' |
+    tr ',' '.' |
+    awk '{sum += $1; n++} END {printf sum/n}'
+)"
+CM_V2="$(
+grep 'cpu_core/branch-misses/u' perfs/perfs_v2.txt |
+    awk '{print $4}' |
+    tr ',' '.' |
+    awk '{sum += $1; n++} END {printf sum/n}'
+ )"
 
 clear
 
@@ -42,6 +101,6 @@ printf "
 Mesure|v0|v1|v1b|v2|\n
 Real Time|${TIME_V0}|${TIME_V1}|${TIME_V1B}|${TIME_V2}|\n
 IPC|${IPC_V0}|${IPC_V1}|${IPC_V1B}|${IPC_V2}|\n
-Cache-misses|${CM_V0}|${CM_V1}|${CM_V1B}|${CM_V2}\n" | column -t -s '|' > results/perfs2.txt
+Branche-miss rate|${CM_V0}|${CM_V1}|${CM_V1B}|${CM_V2}\n" | column -t -s '|' > results/perfs.txt
 
 cat results/perfs.txt
